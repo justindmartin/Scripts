7f31eb62111d444ca0b594ec36a9165934306f08
Justin Martin <justin.d.martin1@gmail.com>
2013-03-16 00:24:17 -0500

    Working Version - 1.0

--- getWebpageLinks.sh
+++ getWebpageLinks.sh
@@ -2,47 +2,45 @@
 #Description: fetches all links from a webpage and outputs to file or console
 #Author: Justin Martin(http://www.1justinmartin.com)
 
-if [[ $1 ]]
+#clear console
+clear
+
+if [[ $1 ]] #if required parameters are set
 then
+	#install lynx, if not already installed
+	echo 'Installing Lynx, if not already installed...'
+	echo
+	sudo apt-get -qq -y install lynx
+
+	#fetch and parse page HTML
 	webpageLinks=`lynx -dump $1 | awk '/http/{print $2}'`
 
-	#if printing to console, then output header
-	if [[ -z $2 ]]
+	if [[ -z $2 ]] #if printing to console
 	then
+		#then output header
 		echo "Links from $1"
 		echo '==================================='
+	else
+		#remove output file, if it already exists
+		rm -f $2
 	fi
 	for webpageLink in $webpageLinks
 	do
-		isLinkDuplicate=0
-		for existingLink in $webpageLinks
-		do
-			if [["$existingLink" == "$webpageLink"]]
-			then
-				$isLinkDuplicate = 1
-			fi
-		done
-		if ["$isLinkDuplicate" == 0]
+		if [[ -z $2 ]]
 		then
-			if [[ -z $2 ]]
-			then
-				echo $webpageLink
-			else
-				echo $webpageLink > $2
-				if [[ $webpageLink != $webpageLinks[0] ]]
-				then
-					echo > $2
-				fi
-			fi
+			echo $webpageLink
+		else
+			echo $webpageLink >> $2
 		fi
 	done
-	#if print to console, then output footer
-	if [[ -z $2 ]]
+
+	if [[ -z $2 ]] #if printing to console
 	then
+		#then output footer
 		echo '==================================='
 	fi
 else
-	clear
+	#print required program parameters
 	echo "bash getWebpageLinks.sh URL OUTPUT_FILE(optional)"
 	echo
 fi
